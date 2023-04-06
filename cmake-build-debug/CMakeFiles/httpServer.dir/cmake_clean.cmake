80e7d7de9cf66f87a468a77bf658681ab7db7f18
Kevin <kevin.hoang.nguyen@hotmail.com>
2023-04-06 01:55:18 -0400

    added database viewer and implemented post request with database

---
+++ cmake-build-debug/CMakeFiles/httpServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/httpServer.dir/main.c.o"
+  "CMakeFiles/httpServer.dir/main.c.o.d"
+  "CMakeFiles/httpServer.dir/tcp.c.o"
+  "CMakeFiles/httpServer.dir/tcp.c.o.d"
+  "httpServer"
+  "httpServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/httpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ndbmDatabase.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ndbmDatabase.dir/ndbm.c.o"
+  "CMakeFiles/ndbmDatabase.dir/ndbm.c.o.d"
+  "ndbmDatabase"
+  "ndbmDatabase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ndbmDatabase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
